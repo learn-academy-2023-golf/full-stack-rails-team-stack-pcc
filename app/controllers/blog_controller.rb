73ff80e416fb4f578ec95122ca1fbc0346ac6b04
TheLavitz <lavitz@gmail.com>
2023-11-28 16:08:12 -0800

    story 3

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -2,9 +2,13 @@ class BlogController < ApplicationController
     def index 
         @posts = Blog.all
     end
+
     def show
       @post = Blog.find(params[:id])  
     end
     
+    def new
+      @post = Blog.new
+    end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 # HTTP verb, URL, hashrocket, controller, method
 get '/posts' => 'blog#index', as: 'posts'
+get '/posts/new' => 'blog#new', as: 'new_post'
 get '/post/:id' => 'blog#show', as: 'post'
 end
 
